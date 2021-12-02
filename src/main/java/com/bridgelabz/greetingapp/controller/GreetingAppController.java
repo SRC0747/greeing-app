0a3de25dc703c14ba7fcf70cf32408bcf70222ce
SRC0747 <RjSrc@0747>
2021-12-03 00:53:33 +0530

    Added : Code extending controller to use Service layer to get greeting message.

--- src/main/java/com/bridgelabz/greetingapp/controller/GreetingAppController.java
+++ src/main/java/com/bridgelabz/greetingapp/controller/GreetingAppController.java
@@ -1,5 +1,7 @@
 package com.bridgelabz.greetingapp.controller;
 
+import com.bridgelabz.greetingapp.service.GreetingAppService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,6 +9,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping
 public class GreetingAppController {
+
+    @Autowired
+    GreetingAppService greetingAppService;
 
     @GetMapping(value = "/message")
     public String getHelloGoodMorningGreetingMessage() {
---
+++ src/main/java/com/bridgelabz/greetingapp/service/GreetingAppService.java
@@ -0,0 +1,10 @@
+package com.bridgelabz.greetingapp.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class GreetingAppService {
+    public String greet() {
+        return "Hello World";
+    }
+}
