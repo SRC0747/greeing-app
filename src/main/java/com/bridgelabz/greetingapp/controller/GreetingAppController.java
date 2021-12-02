3a6da2c923928841b17f0c2f364ea13196e009c3
SRC0747 <RjSrc@0747>
2021-12-03 02:06:58 +0530

    Added : Code for getting greeting message of Hello World from service.

--- src/main/java/com/bridgelabz/greetingapp/controller/GreetingAppController.java
+++ src/main/java/com/bridgelabz/greetingapp/controller/GreetingAppController.java
@@ -1,20 +1,29 @@
 package com.bridgelabz.greetingapp.controller;
 
+import com.bridgelabz.greetingapp.dto.GreetingAppDTO;
 import com.bridgelabz.greetingapp.service.GreetingAppService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping
+@RequestMapping("/hello")
 public class GreetingAppController {
 
     @Autowired
     GreetingAppService greetingAppService;
 
-    @GetMapping(value = "/message")
-    public String getHelloGoodMorningGreetingMessage() {
-        return "Hello Good Morning";
+    @GetMapping("")
+    public GreetingAppDTO getHelloMessageFromDTO(
+            @RequestBody GreetingAppDTO greetingAppDTO){
+        return greetingAppDTO;
     }
+
+    @GetMapping(value = "/message")
+    public String getGreetingMessageOfHelloWorld(){
+        return greetingAppService.greet();
+    }
+    
 }
